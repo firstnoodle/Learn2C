688aa7f1aad44bea48ac57c03b921c553b855b96
sebastian thielke <seb@redboatopera.com>
2019-01-19 16:55:55 +0100

    changed background to yellow

--- src/main.c
+++ src/main.c
@@ -33,14 +33,22 @@ int main(int argc, char *argv[]) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     } else {
         //Create window
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        window = SDL_CreateWindow(
+			"SDL Tutorial", 
+			SDL_WINDOWPOS_UNDEFINED, 
+			SDL_WINDOWPOS_UNDEFINED, 
+			SCREEN_WIDTH, 
+			SCREEN_HEIGHT, 
+			SDL_WINDOW_SHOWN
+		);
+
         if( window == NULL ) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         } else {
             //Get window surface
             screenSurface = SDL_GetWindowSurface(window);
             //Fill the surface white
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ));
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 255, 255, 0 ));
             //Update the surface
             SDL_UpdateWindowSurface(window);
             //Wait two seconds

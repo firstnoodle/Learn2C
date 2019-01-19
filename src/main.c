24060210a2a197dcce1e399bdd53a1dd5e48e632
sebastian thielke <seb@redboatopera.com>
2019-01-19 16:25:45 +0100

    main.c: including SDL and added basic window init code

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+
+#include <SDL2/SDL.h>
+
 #include "hellomake.h"
 
 #define PI 3.14
 
-int main() {
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char *argv[]) {
 	// call a function in another file
 	myPrintHelloMake();
 
@@ -15,5 +22,35 @@ int main() {
 	ret = acos(x) * val;
 	printf("The arc cosine of %lf is %lf degrees\n", x, ret);
 
-	return(0);
+	// SDL
+	//The window we'll be rendering to
+    SDL_Window* window = NULL;
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+        //Create window
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if( window == NULL ) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        } else {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface(window);
+            //Fill the surface white
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ));
+            //Update the surface
+            SDL_UpdateWindowSurface(window);
+            //Wait two seconds
+            SDL_Delay(10000);
+        }
+    }
+	//Destroy window
+    SDL_DestroyWindow(window);
+    //Quit SDL subsystems
+    SDL_Quit();
+
+	return 0;
 }
